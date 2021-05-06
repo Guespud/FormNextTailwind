7dbafe93587365bbedf55777c1aff1715f7bb3fd
Alejandro Guespud <alejo.habbacuc@gmail.com>
2021-05-05 20:32:36 -0500

    conf inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return <div>
+    <h1>INICIO</h1>
+  </div>;
+}
